8871af6293cff6bd0626a4674f3d60ec565c9f4d
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-02-01 15:38:32 +0000

    First commit - algorithm working

---
+++ bubble.js
@@ -0,0 +1,37 @@
+
+let inputNumbers = document.getElementById("userInput");
+const checkButton = document.getElementById("checkNumbers")
+
+let userArray = null;
+
+checkButton.addEventListener("click", function(){
+    userArray = inputNumbers.value.split('').map(Number);
+    let result = sortArray(userArray, arraySortedYesNo);
+    window.alert(result);
+})
+
+function arraySortedYesNo(uncertain){
+    for (before = 0; before < uncertain.length; before++){
+        if (uncertain[before] > uncertain[before+1]){
+            return false
+        }
+    }
+    return true
+}
+
+function sortArray(array, checker){
+    while(checker(array) === false){
+        let pointer = 0;
+        for(let num = 0; num < array.length; num++){
+            for (let point = pointer; array[point] > array[point + 1]; point++){
+                let bridgeValue = array[point]
+                array[point] = array[point + 1]
+                array[point + 1] = bridgeValue
+            }
+            pointer++
+        }
+    }
+    return array
+}
+
+//console.log(sortArray([11,4,5,8,7,1,6,2,3], arraySortedYesNo))
