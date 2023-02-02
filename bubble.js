92a86ec7f16cc8108d0128dfe2f96e9c4e0c839f
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-02-02 01:13:44 +0000

    Array visible to user can now be modified. Need to adjust

--- bubble.js
+++ bubble.js
@@ -3,13 +3,37 @@ let inputNumbers = document.getElementById("userInput");
 const checkButton = document.getElementById("checkNumbers")
 const boxNumbers = document.getElementById("number-container")
 
+let boxesReference = []
+
+
+inputNumbers.addEventListener("keyup", function(){
+    let lastInput = inputNumbers.value.split('')[inputNumbers.value.length-1]
+    let littleBox = document.createElement("div");
+    littleBox.classList.add("number-boxes");
+    littleBox.innerText = lastInput;
+    boxNumbers.appendChild(littleBox);
+    boxesReference.push(littleBox);
+    //boxNumbers.children[4].innerText = "BOBO";
+    //console.log("Lets see ", boxNumbers.children[4])
+});
+
+
 let userArray = null;
 
 checkButton.addEventListener("click", function(){
     userArray = inputNumbers.value.split('').map(Number);
-    let result = sortArray(userArray, arraySortedYesNo);
-    window.alert(result);
+    let result = sortArray(userArray, arraySortedYesNo, boxNumbers, reviewInside);
 })
+
+function reviewInside(objectBox, w){
+    //console.log ("-just test ", typeof objectBox.children[w].innerText)
+    //objectBox.children[w].innerText = "BEBS"
+    let bridge = objectBox.children[w].innerText;
+    objectBox.children[w].innerText = objectBox.children[w+1].innerText
+    objectBox.children[w+1].innerText = bridge
+
+}
+
 
 //Bubble sort algorithm functions
 
@@ -22,7 +46,7 @@ function arraySortedYesNo(uncertain){
     return true
 }
 
-function sortArray(array, checker){
+function sortArray(array, checker, boxes, printer){
     while(checker(array) === false){
         let pointer = 0;
         for(let num = 0; num < array.length; num++){
@@ -30,6 +54,7 @@ function sortArray(array, checker){
                 let bridgeValue = array[point]
                 array[point] = array[point + 1]
                 array[point + 1] = bridgeValue
+                printer(boxes, point)
             }
             pointer++
         }
@@ -40,11 +65,6 @@ function sortArray(array, checker){
 //Graphic part functions
 
 
-inputNumbers.addEventListener("keyup", function(){
-    console.log(inputNumbers.value.split('')[inputNumbers.value.length-1])
-    let littleBox = document.createElement("div");
-    littleBox.innerText = inputNumbers.value.split('')[inputNumbers.value.length-1]
-    littleBox.classList.add("number-boxes");
-    boxNumbers.appendChild(littleBox);
-    
-});
+
+
+
