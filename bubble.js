734d43a5ce0da909e908cde738acec1c25ff0a2a
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-02-06 01:38:47 +0000

    Timer format updated, reset button added, and refactor js

--- bubble.js
+++ bubble.js
@@ -1,14 +1,14 @@
 
 let inputNumbers = document.getElementById("userInput");
-const checkButton = document.getElementById("checkNumbers");
-const boxNumbers = document.getElementById("number-container");
+const sortButton = document.getElementById("checkNumbers");
+const numbContainer = document.getElementById("number-container");
 const timerClock = document.getElementById("timer");
-const reload = document.getElementById("loadAgain")
+const reloadPage = document.getElementById("loadAgain")
 
-let boxesReference = []
-
-reload.addEventListener("click", function() {
-    location.reload();
+reloadPage.addEventListener("click", function() {
+    setTimeout(function(){
+        location.reload();
+    }, 500);
 });
 
 inputNumbers.addEventListener("keyup", function(){
@@ -16,16 +16,14 @@ inputNumbers.addEventListener("keyup", function(){
     let littleBox = document.createElement("div");
     littleBox.classList.add("number-boxes");
     littleBox.innerText = lastInput;
-    boxNumbers.appendChild(littleBox);
-    boxesReference.push(littleBox);
+    numbContainer.appendChild(littleBox);
 });
-
 
 let userArray = null;
 
-checkButton.addEventListener("click", function(){
+sortButton.addEventListener("click", function(){
     userArray = inputNumbers.value.split('').map(Number);
-    let result = sortArray(userArray, arraySortedYesNo, boxNumbers, reviewInside, updateClock, timerClock, reload);
+    sortArray(userArray, arraySortedYesNo, numbContainer, reviewInside, updateClock, timerClock, reloadPage);
 })
 
 
@@ -71,62 +69,64 @@ function printerWithTimeout(boxes, print, point, plusTime, start, maxTime, reloj
         }, 1000 * plusTime)
     });
 }
-/*
+
+//Populate clock container with seconds and milliseconds
+function paintLessOneSecond(counter, clockBox){
+    let intervalId = setInterval(function() {
+        counter++;
+        if (counter >= 100) {
+            clearInterval(intervalId);
+            return;
+        }
+        updated = "00"  + ":" + counter.toString().padStart(2, "0");
+        clockBox.innerText = updated;
+    }, 10);
+}
+
+function paintSecondCase(counter, clockBox, timeStr){
+    let intervalId = setInterval(function() {
+        counter++;
+        if (counter >= 101) {
+            clearInterval(intervalId);
+            updated = "0" + timeStr.substring(0,1) + ":" + "00";
+            clockBox.innerText = updated;
+            return;
+        }else{
+            updated = "0" + timeStr.substring(0,1) + ":" + counter.toString().padStart(2, "0");
+            clockBox.innerText = updated;
+        }  
+    }, 10);
+}
+
+function paintThirdCase(counter, clockBox, timeStr){
+    let intervalId = setInterval(function() {
+        counter++;
+        if (counter >= 101) {
+            clearInterval(intervalId);
+            updated = timeStr.substring(0,2) + ":" + "00";
+            clockBox.innerText = updated;
+            return;
+        }else{
+            updated = timeStr.substring(0,2) + ":" + counter.toString().padStart(2, "0");
+            clockBox.innerText = updated;
+        }
+    }, 10);
+}
+
 function updateClock(time, clock) {
     let countTo99 = 0;
     let updated = "";
-
     let timeString = time.toString().padStart(4, "0");
-    if (time < 1000) {
-        updated = "00:" + timeString.substring(0,2);
-    } else if (time > 1000 && time < 10000){
-        updated = "0" + timeString.substring(0,1) + ":" + timeString.substring(1,3);
-    }else if (time > 10000){
-        updated = timeString.substring(0,2) + ":" + timeString.substring(2,4)
-    }
-    clock.innerText = "00:"+updated;    
-}*/
-
-
-function updateClock(time, clock) {
-    let countTo99 = 0;
-    let updated = "";
-    let timeString = time.toString().padStart(4, "0");
-
     if (time < 1000){
-        let intervalId = setInterval(function() {
-            countTo99++;
-            if (countTo99 >= 100) {
-                clearInterval(intervalId);
-                return;
-            }
-            updated = "00"  + ":" + countTo99.toString().padStart(2, "0");
-            clock.innerText = updated;
-        }, 10);
+        paintLessOneSecond(countTo99, clock)
     } else if (time > 1000 && time < 10000){
-        let intervalId = setInterval(function() {
-            countTo99++;
-            if (countTo99 >= 100) {
-                clearInterval(intervalId);
-                return;
-            }
-            updated = "0" + timeString.substring(0,1) + ":" + countTo99.toString().padStart(2, "0");
-            clock.innerText = updated;
-        }, 10);
+        paintSecondCase(countTo99, clock, timeString)
     } else if(time > 10000){
-        let intervalId = setInterval(function() {
-            countTo99++;
-            if (countTo99 >= 100) {
-                clearInterval(intervalId);
-                return;
-            }
-            updated = timeString.substring(0,2) + ":" + countTo99.toString().padStart(2, "0");
-            clock.innerText = updated;
-        }, 10);
+        paintThirdCase(countTo99, clock, timeString)
     }
 }
 
-  
+//Main function organizing the array and controlling the display of the array while changing
 function sortArray(array, checker, boxes, printer, watch, screen, rest){
     let start = performance.now(), plusTime = 0, maxElapsedTime = 0, promises = [];
     while(checker(array) === false){
@@ -136,7 +136,8 @@ function sortArray(array, checker, boxes, printer, watch, screen, rest){
                 switchIndex(point, array)
                 printerWithTimeout(boxes, point, plusTime, start, maxElapsedTime, watch, screen)
                 plusTime++;
-                promises.push(printerWithTimeout(boxes,printer, point, plusTime, start, maxElapsedTime, watch,screen).then((newMaxTime) => {
+                promises.push(printerWithTimeout(boxes,printer, point, plusTime, start, maxElapsedTime, watch,screen)
+                .then((newMaxTime) => {
                     maxElapsedTime = newMaxTime;
                 }));
             }
@@ -144,21 +145,11 @@ function sortArray(array, checker, boxes, printer, watch, screen, rest){
         } 
     }
     Promise.all(promises).then(() => {
-        console.log("Max Time passed: ", (maxElapsedTime/1000).toFixed(1), " Array: ", array)
         rest.style.visibility = "visible";
-
     });
-
-
-    
-    return array
 }
 
 
-
-
-
-//Graphic part functions
 
 
 
